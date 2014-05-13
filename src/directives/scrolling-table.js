60ec4080728a782a60212d472d4eef7d4b56beb8
jadrake75 <jadrake75@gmail.com>
2014-05-12 23:14:46 -0500

    Initial file check-in
    "Untested"

---
+++ src/directives/scrolling-table.js
@@ -0,0 +1,65 @@
+(function(angular) {
+
+	'use strict';
+    var tables = angular.module('scrolling-table', []);
+	
+	tables.directive('scrollingTable', function($timeout, ControllerEvents) {
+        var linker = function(scope, element, attrs) {
+            var initialized = false;
+            var modelData = (attrs.data) ? attrs.data : 'data';
+            var wrapper = element.wrap('<div class="tableWrapper"><div class="scroller"></div></div>').parents('.tableWrapper');
+            wrapper.resize(function() {
+                calculateDimensions(wrapper);
+            });
+            var headWrap = $(document.createElement('div'))
+                    .addClass('tableHeader')
+                    .prependTo(wrapper);
+            $(document.createElement('table'))
+                    .appendTo(headWrap)
+                    .css("width", "100%");
+
+            var recalcFn = function() {
+                $timeout(function() {
+                    calculateDimensions(wrapper);
+                }, 50);
+            };
+
+            scope.$watch(modelData, function() {
+                recalcFn();
+            });
+
+            scope.$on(ControllerEvents.filter, function(e) {
+                recalcFn();
+            });
+        };
+        function calculateDimensions(wrapDiv) {
+            var header = wrapDiv.find('thead');
+            var body = wrapDiv.find("tbody");
+            var innerWrap = body.parents('div.scroller');
+            var headerTable = wrapDiv.find('div.tableHeader table');
+            headerTable.append(header);
+            var h = header.find('tr').height();
+            h = (h > 0) ? h : 25;
+            innerWrap.height(wrapDiv.height() - h);
+            calculateWidths(wrapDiv);
+        }
+
+        function calculateWidths(table) {
+            var allBodyCols = table.find('tbody tr:first td');
+            var scroller = table.find('div.scroller');
+            table.find('.tableHeader table').width("calc(100% - " + (scroller.width() - scroller.find('table').width()) + "px)");
+            if (allBodyCols.length > 0) {
+                table.find('tr th').each(function(index) {
+                    var padding = 0;
+                    var desiredWidth = $(allBodyCols[index]).width() - padding + (( index === 0 ) ? -2 : 0);
+                    $(this).width(desiredWidth);
+                });
+            }
+        }
+        return {
+            restrict: 'A',
+            link: linker
+        };
+    });
+	
+})(angular);
