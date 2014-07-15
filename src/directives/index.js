fdbe00bdfab94d60eab58926284a3588e9b02309
jadrake75 <jadrake75@gmail.com>
2014-07-15 11:46:59 -0500

    Add resizing of columns with column groups
    Add the ability to resize columns (except last columns)

--- src/directives/index.js
+++ src/directives/index.js
@@ -6,7 +6,8 @@
         'table.scrolling-table', 
         'table.empty-table',
         'table.table-selector',
-        'table.highlightColumn'
+        'table.highlightColumn',
+        'table.column-resizing'
     ]);
     
 })(angular);
